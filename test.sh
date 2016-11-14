c22d0935af7e01c8c1cd513956d88e28ee25d12c
Raul Ferreira <raulmendesferreira@gmail.com>
2016-11-14 19:39:30 +0000

    Better test file

--- test.sh
+++ test.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-# set -x
+set -x
+
+mkdir -p ~/DFS/A/1
+mkdir -p ~/DFS/A/2
+mkdir -p ~/DFS/B/1
+mkdir -p ~/DFS/B/2
+mkdir -p ~/DFS/C/1
+mkdir -p ~/DFS/logs
 
 cd outd
 rmiregistry&
-java fcup.MetadataServer
+java fcup.MetadataServer &> ~/DFS/logs/Meta.log &
+sleep 1
+java fcup.StorageServer ~/DFS/A / localhost &> ~/DFS/logs/Storage1.log &
+sleep 1
+java fcup.StorageServer ~/DFS/B /B localhost &> ~/DFS/logs/Storage2.log &
+sleep 1
+java fcup.StorageServer ~/DFS/C /C localhost &> ~/DFS/logs/Storage3.log &
+sleep 1
+java fcup.Client ../apps.conf localhost
